acc91387f93b78f1a1df647690df28f3ad3bf535
Se Joon Park <sparky9043@gmail.com>
2026-03-05 16:40:13 +0900

    feat: create helper function that alters modified_html and returns the html

--- html_editing.py
+++ html_editing.py
@@ -25,6 +25,11 @@ def create_website(template_file, html_output):
     
     modified_html = modified_html.replace("</head>", f"{daisy_ui}</head>")
     
+    # Helper function to add element before body tag and return modified_html
+    def add_to_body(modified_html, element_to_add):
+        modified_html = modified_html.replace("</body>", f"{element_to_add}</body>")
+        return modified_html
+    
     # Apply Theme
     data_theme = "halloween"
     modified_html = modified_html.replace('<html lang="en">',
@@ -32,8 +37,26 @@ def create_website(template_file, html_output):
     
     # Add Title to Body
     h1_tag = "<h1>Welcome to My Website</h1>"
-    modified_html = modified_html.replace("</body>", f"{h1_tag}</body>")
+    modified_html = add_to_body(modified_html, h1_tag)
 
+    # Add Card to HTML
+    card = """
+    <div class="card bg-base-100 w-96 shadow-sm">
+    <figure>
+        <img
+        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+        alt="Shoes" />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">Card Title</h2>
+        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+    </div>
+    </div>
+    """
+    modified_html = add_to_body(modified_html, card)
     
     # Write index.html file after updating with Python
     with open(html_output, "w") as file:
